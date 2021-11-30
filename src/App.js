283207fe45fd292b17fc59f7c2d893880b35a511
Peter Conley <peterdavidconley@gmail.com>
2021-11-30 15:55:57 -0500

    rendering my profile photo and profile information to the DOM

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import User from './components/User'
 import FollowerList from './components/FollowerList'
 import axios from 'axios'
 import './App.css'
-class App extends React.Component {
 
+class App extends React.Component {
 
   state = {
     currentuser: '',
@@ -18,14 +18,27 @@ class App extends React.Component {
 
     axios.get(`https://api.github.com/users/peterdavidconley`)
     .then(resp => {
-      console.log(resp)
+      this.setState({
+        ...this.state,
+        profile: resp.data,
+      })
     })
     .catch(err => {
       console.error(err)
     })
 
-
     // Axios call for followers list
+
+    axios.get('https://api.github.com/users/peterdavidconley/followers')
+    .then(resp => {
+      this.setState({
+        ...this.state,
+        followers: resp.data,
+      })
+    })
+    .catch(err => {
+      console.log(err)
+    })
 
   }
 
@@ -43,7 +56,7 @@ class App extends React.Component {
         </form>
       </section>
       <br/>
-      <User/>
+      <User profile={this.state.profile}/>
       <br/>
       <h2>FOLLOWERS:</h2>
       <br/>
--- src/components/User.js
+++ src/components/User.js
@@ -9,13 +9,13 @@ class User extends React.Component{
         <div>
           <section className='user'>
             <div>
-              <img height='200px' width='200px' src='https://via.placeholder.com/450'/>
+              <img height='200px' width='200px' src={this.props.profile.avatar_url}/>
             </div>
             <div>
-              <h3>Name:</h3>
-              <h3>Username: </h3>
-              <h4>TOTAL REPOS: </h4>
-              <h4>TOTAL FOLLOWERS: </h4>
+              <h3>Name: {this.props.profile.name}</h3>
+              <h3>Username: {this.props.profile.login}</h3>
+              <h4>TOTAL REPOS: {this.props.profile.public_repos}</h4>
+              <h4>TOTAL FOLLOWERS: {this.props.profile.followers}</h4>
             </div>
           </section>
           
